2c5af7df8ad22dff24267684b98880111a587c6f
YoctoByte <David.vtwout@gmail.com>
2016-06-29 21:58:38 +0200

    commit before changing htmlparser

---
+++ htmlparser.py
@@ -0,0 +1,212 @@
+import requests
+
+non_paired_tags = ['area', 'base', 'br', 'col', 'command',
+                   'embed', 'hr', 'img', 'input', 'link',
+                   'meta', 'param', 'source', 'wbr', 'keygen',
+                   'track', 'button']
+
+
+def parse_from_url(url):
+    page = requests.get(url)
+    html_string = page.content.decode('utf-8')
+    return parse_from_string(html_string)
+
+
+def parse_from_file(filename):
+    with open(filename, 'r') as file:
+        html_string = file.read()
+        file.close()
+    return parse_from_string(html_string)
+
+
+def parse_from_string(html_string):
+    def to_tag_strings(html_str):
+        left_pos = html_str.find('<')
+        while left_pos != -1:
+            right_pos = html_str.find('>', left_pos)
+            if right_pos == -1:
+                raise ValueError('String could not be parsed as HTML text. Last tag has no ending')
+            tag_str = html_str[left_pos+1:right_pos]
+            next_left = html_str.find('<', right_pos)
+            content_str = html_str[right_pos+1:next_left]
+            yield tag_str, content_str
+            left_pos = next_left
+
+    # Compress the string to a single line:
+    new_string = ''
+    for line in html_string.split('\n'):
+        new_string += line.strip()
+    html_string = new_string
+
+    # begin parsing the string:
+    depth = 0
+    open_tags = dict()  # elements which are not closed yet
+    awaiting_elements = list()  # elements which are closed, but are waiting until depth decreases
+    for tag_string, plain_text in to_tag_strings(html_string):
+        element = _parse_to_element(tag_string, plain_text)
+
+        if element.name[0] == '/':  # if element is closing tag
+            depth -= 1
+            element.name = element.name[1:]
+            try:
+                starting_element = open_tags[element.name].pop()
+            except KeyError:
+                raise ValueError('String could not be parsed as HTML text. '
+                                 'It contains ending tags without corresponding starting tags')
+            starting_element.depth = depth
+
+            new_awaiting_elements = list()
+            for awaiting_element in awaiting_elements:
+                if awaiting_element.depth > depth:
+                    starting_element.content.append(awaiting_element)
+                else:
+                    new_awaiting_elements.append(awaiting_element)
+            awaiting_elements = new_awaiting_elements
+            awaiting_elements.append(starting_element)
+        else:  # if element is not a closing tag
+            if element.name == '!DOCTYPE':
+                continue
+            if element.paired:
+                depth += 1
+                if element.name in open_tags:
+                    open_tags[element.name].append(element)
+                else:
+                    open_tags[element.name] = [element]
+            else:
+                element.depth = depth
+                awaiting_elements.append(element)
+    return awaiting_elements.pop()
+
+
+def _parse_to_element(tag_string, text=''):
+    attribute_strings = tag_string.split(' ')
+    tag_name = attribute_strings.pop(0)
+
+    if tag_name in non_paired_tags:
+        paired = False
+    else:
+        paired = True
+
+    # Parse the attributes of the tag:
+    attributes_dict = dict()
+    for attribute in attribute_strings:
+        try:
+            key, value = attribute.split('=', 1)
+        except ValueError:
+            key, value = attribute, ''
+        attributes_dict[key] = value.strip('"')
+
+    return Element(name=tag_name, text=text, attributes=attributes_dict, paired=paired)
+
+
+class Element:
+    def __init__(self, name='', depth=-1, attributes=None, content=None, paired=None, text=''):
+        self.attributes = dict()
+        self.content = list()
+        self.name = name
+        self.depth = depth
+        self.paired = paired
+        self.text = text
+        if attributes:
+            self.attributes = attributes
+        if content:
+            self.content = content
+
+    def get_elements(self, elem_name, recursive=True):
+        elements = list()
+        for content in self.content:
+            if content.name == elem_name:
+                elements.append(content)
+            if recursive:
+                elements.extend(content.get_elements(elem_name))
+        return elements
+
+    def remove(self, name=None, attribute=None, recursive=True):
+        # First some pre-processing of the arguments:
+        if not name and not attribute:
+            return
+
+        # The function that check whether or not the attribute is in the content item:
+        def attr_match(cont_attrs):
+            if not attribute:
+                return True
+            if not cont_attrs:
+                return False
+            try:
+                key, value = attribute[0], attribute[1]
+            except IndexError:
+                raise ValueError('attributes should be a pair (tuple, list, .. ).')
+            if key == '*' and value == '*':
+                return True
+            if key == '*':
+                for k in cont_attrs:
+                    if cont_attrs[k] == value:
+                        return True
+                return False
+            else:
+                if key in cont_attrs and value == '*':
+                    return True
+                elif key in cont_attrs:
+                    if cont_attrs[key] == value:
+                        return True
+                    else:
+                        return False
+                else:
+                    return False
+
+        for i, content in enumerate(self.content):
+            if (content.name == name or not name) and attr_match(content.attributes):
+                del self.content[i]
+                continue
+            if recursive:
+                content.remove(name=name, attribute=attribute, recursive=recursive)
+
+    def append(self, item):
+        self.content.append(item)
+
+    def extend(self, item):
+        self.content.extend(item)
+
+    def __iter__(self):
+        for item in self.content:
+            yield item
+
+    def __getitem__(self, index):
+        return self.content[index]
+
+    def to_text(self):
+        text = self.text
+
+        if self.name == 'br':
+            text = '\n' + text
+        if self.name == 'title':
+            return ''
+
+        for content in self.content:
+            text += content.to_text()
+        return text
+
+    def __str__(self):
+        def indent(string):
+            new_string = ''
+            for line in (line for line in string.split('\n') if line):
+                line = '    ' + line + '\n'
+                new_string += line
+            return new_string
+
+        content_string = ''
+        for element in self:
+            content_string += str(element)
+        content_string = indent(content_string)
+
+        attr_string = ''
+        for attr in self.attributes:
+            attr_string += ' ' + attr + '="' + self.attributes[attr] + '"'
+
+        text = self.text
+
+        end_tag = ''
+        if self.paired:
+            end_tag = '</' + self.name + '>\n'
+
+        return '<' + self.name + attr_string + '>\n' + text + content_string + end_tag
---
+++ wikiparser.py
@@ -0,0 +1,65 @@
+from htmlparser import parse_from_url
+import json
+import requests
+
+
+def parse_tables_to_json(url):
+    json_data = list()
+    page = parse_from_url(url)
+    page_raw = requests.get(url)
+    html_string = page_raw.content.decode('utf-8')
+    with open('files/isotopes/' + url.split('/')[-1] + '.html', 'w') as file:
+        file.write(html_string)
+    tables = page.get_elements('table')
+    for table in tables:
+        table.remove(attribute=('style', 'display:none;'))
+        table.remove(attribute=('style', 'display:none'))
+        print(table)
+
+        table_data = list()
+        for row in table:
+            if row[0].name in ['td', 'th']:
+                values = parse_row(row)
+                table_data.append(values)
+        json_data.append(table_data)
+
+    with open('files/isotopes/' + url.split('/')[-1] + '.json', 'w') as jsonfile:
+        json.dump(json_data, jsonfile, separators=(',', ':'), indent=4)
+
+
+def parse_row(row):
+    values = list()
+    for item in row:
+        value = item.to_text()
+        values.append(_parse_value(value))
+    return values
+
+
+def _parse_value(string):
+    if string in ['–', '']:
+        return None
+    string = string.replace('\n', '')
+    old_string = string
+
+    # left, right = string.find('('), string.find(')')
+    # if left != -1 and right != -1:
+    #     if left == 0:
+    #         string == string[1:-1]
+    #     else:
+    #         string = string[0:left-1]
+    #         # todo: significance
+
+    # if string[0] == '[' and string[-1] == ']':
+    #     string = string[1:-1]
+    #     # todo: estimate
+
+    try:
+        value = float(string)
+        if value.is_integer():
+            value = int(value)
+    except ValueError:
+        value = old_string
+
+    return value
+
+parse_tables_to_json('https://en.wikipedia.org/wiki/Isotopes_of_hydrogen')
