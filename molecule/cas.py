2d01ec73c6bda4159deebd638f3684fc0ee577c3
YoctoByte <David.vtwout@gmail.com>
2016-08-13 14:19:10 +0200

    started with iupac

--- molecule/cas.py
+++
@@ -1,7 +0,0 @@
-# todo:
-def parse_from(molecule, cas_number):
-    pass
-
-# todo:
-def parse_to(molecule):
-    pass
--- pychem/molecules/iupac.py
+++ pychem/molecules/iupac.py
@@ -1,6 +1,11 @@
-# from molecule.molecules import Molecule
+import json
+import os
 
-precedence = list()
+parent_dir = os.path.split(os.path.dirname(__file__))[0]
+DATA_DIR = parent_dir + '/data/iupac/'
+
+CARBON_PREFIXES = None
+
 
 def parse_from(molecule, name_string):
     pass
@@ -12,8 +17,27 @@ def parse_to(molecule):
 
 
 def _find_parent_chain(molecule):
+    # find all longest carbon chains
+    # 1. It should have the maximum number of substituents of the suffix functional group. By suffix, it
+    # is meant that the parent functional group should have a suffix, unlike halogen substituents. If
+    # more than one functional group is present, the one with highest precedence should be used.
+    # 2. It should have the maximum number of multiple bonds
+    # 3. It should have the maximum number of single bonds.
+    # 4. It should have the maximum length.
     pass
 
 
 def _find_side_chains(molecule, parent_chain):
     pass
+
+
+def _load_carbon_prefixes():
+    with open(DATA_DIR + 'carbon_prefixes.json') as jf:
+        carbon_prefixes = json.load(jf)
+    for _, prefix in carbon_prefixes.items():
+        print(prefix)
+    with open(DATA_DIR + 'carbon_prefixes2.json', 'w') as jf:
+        json.dump(carbon_prefixes, jf, separators=(',', ':'), indent=2, sort_keys=True)
+
+
+_load_carbon_prefixes()
